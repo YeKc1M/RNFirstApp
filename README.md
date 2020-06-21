79d879d294c2b958d145fa536e2eb932365886da
YeKc1M <383250208@qq.com>
2020-06-21 14:44:11 +0800

    ViewPager添加翻页事件

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # init RN project
 `npx react-native init ProjectName`
+# enable ViewPager
+`yarn add @react-native-community/viewpager`
--- ViewPage.js
+++ ViewPage.js
@@ -1,17 +1,24 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, Alert } from 'react-native'
 import ViewPager from '@react-native-community/viewpager'
 
 const ViewPage=()=>{
+    const [currentPage, setCurrentPage]=useState(0)
     return(
         <>
         <View><Text>page view</Text></View>
-        
-        <ViewPager initialPage={0} style={{flex:1}}>
-            <View key="1" style={{borderWidth:1, backgroundColor: 'red'}}><Text>page 1</Text></View>
-            <View key="2" style={{borderWidth:1, backgroundColor: 'yellow'}}><Text>page 2</Text></View>
+        <View style={{flexDirection:'row'}}>
+            <Text style={currentPage==0?{margin:2, backgroundColor:'gray'}:{margin:2}}>page1</Text>
+            <Text style={currentPage==1?{margin:2, backgroundColor:'gray'}:{margin:2}}>page2</Text>
+            <Text style={currentPage==2?{margin:2, backgroundColor:'gray'}:{margin:2}}>page3</Text>
+        </View>
+        <ViewPager initialPage={currentPage} style={{flex:1, margin: 5}} scrollEnabled={true}
+            onPageSelected={(e)=>{setCurrentPage(e.nativeEvent.position)}}>
+            <View key="1" style={{borderWidth:1}}><Text>page {currentPage+1}</Text></View>
+            <View key="2" style={{borderWidth:1}}><Text>page {currentPage+1}</Text></View>
+            <View key="3" style={{borderWidth:1}}><Text>page {currentPage+1}</Text></View>
         </ViewPager> 
-        
+        <View><Text>after view pager</Text></View>
         </>
     )
 }
